c86c3368a1e01f52411d7db54d2d5d9489f54a37
ilyasparra <Ilyas.Parra1@marist.edu>
2022-11-08 21:12:26 -0500

    Completed Project 3

--- adventure.py
+++ adventure.py
@@ -3,6 +3,7 @@
 #You are an aspiring soccer player and got invited to visit each stadium for every top soccer team in the Premier League! 
 
 import random
+Start = True
 score = 0
 Next_Stop = 1
 user_name = ""
@@ -16,17 +17,17 @@ begin = "Press enter to continue to the next stadium: "
 end = "You have finished your trip! Press Enter to see credits"
 Quit_Msg = "You have decided to quit the game. You will be exited out of the game after completing the stop."
 Quit_Status = False
-msg_1 = "Press enter to continue:"
-msg_2 = "You are at Old Trafford surrounded by red jerseys of Manchester United fans."
-msg_3 = "You are at the Etihad surrounded by Manchester City fans."
-msg_4 = "You are currently at Villa Park surrounded by Aston Villa fans."
-msg_5 = "You are standing in front of the Emirates in front of plenty of Arsenal fans."
-msg_6 = "You gaze upon Anfield with Liverpool fans around you."
-msg_7 = "You visit the King Power Staidum surrounded by Leicester City fans"
-msg_8 = "You are at Stamford Bridge with the loud Chelsea fans."
-msg_9 = "You are at the well known Goodison Park with Everton fans."
-msg_10 = "You have reached Selhurst Park, the stadium for Crystal Palace."
-msg_11 = "You are at the home of West Ham United, London Stadium."
+msg_1 = "\nPress enter to continue:"
+msg_2 = "\nYou are at Old Trafford surrounded by red jerseys of Manchester United fans."
+msg_3 = "\nYou are at the Etihad surrounded by Manchester City fans."
+msg_4 = "\nYou are currently at Villa Park surrounded by Aston Villa fans."
+msg_5 = "\nYou are standing in front of the Emirates in front of plenty of Arsenal fans."
+msg_6 = "\nYou gaze upon Anfield with Liverpool fans around you."
+msg_7 = "\nYou visit the King Power Staidum surrounded by Leicester City fans"
+msg_8 = "\nYou are at Stamford Bridge with the loud Chelsea fans."
+msg_9 = "\nYou are at the well known Goodison Park with Everton fans."
+msg_10 = "\nYou have reached Selhurst Park, the stadium for Crystal Palace."
+msg_11 = "\nYou are at the home of West Ham United, London Stadium."
 Locations = ["Old Trafford","Etihad","Villa Park","Emirates","Anfield","King Power", "Stanford Bridge","Goodison Park","Selhurst Parl","London Stadium"]
 ministop1_input_text = "Press enter to buy a jersey: "
 ministop1_output_text = "The jersey is red and super nice."
@@ -88,38 +89,95 @@ def Command_Check(s):
         print(Quit_Status)
         #x = input(f"{msg_1}")
         #Command_Check(x)
-def Find_Surrounding_Locations(c):
+def Find_Next_Stop():
+    global Next_Stop
+    Break_Indicator = False
     current_coords = [None,None]
-    for i in len(Map):
-        for j in len(Map[j]):
-            if Map[i][j] == c:
+    for i in range(len(Map)):
+        for j in range(len(Map[i])):
+            if Map[i][j] == Next_Stop:
                 current_coords[0] = i
                 current_coords[1] = j
+                Break_Indicator = True
+                break
+        if Break_Indicator:
             break
+        print(f"Coords: {Map[i][j]}\nRow {current_coords[0]}\nCol {current_coords[1]}")
+        
     surrounding_coords = [[],[],[],[]]
-# Find surroudning coordinates, 1st is North, 2nd is South, 3rd is East, 4th is West
-# Display options in begin for next stop
-# Player chooses option as next stop, which is used for the next 
-
-
-
+    
+  
+    
+    if current_coords[0] == 0 or Map[current_coords[0]-1][current_coords[1]] == "N":
+        surrounding_coords[0] = "None"
+    else:  
+        #North
+        surrounding_coords[0] = Locations_Dict[Map[current_coords[0]-1][current_coords[1]]]["Name"]
+    if current_coords[0] == 2 or Map[current_coords[0]+1][current_coords[1]] == "N":
+        surrounding_coords[1] = "None"
+    else:  
+        #South
+        surrounding_coords[1] = Locations_Dict[Map[current_coords[0]+1][current_coords[1]]]["Name"]
+    if current_coords[1] == 0 or Map[current_coords[0]][current_coords[1]-1] == "N":
+        surrounding_coords[2] = "None"
+    else: 
+        #West 
+        surrounding_coords[2] = Locations_Dict[Map[current_coords[0]][current_coords[1]-1]]["Name"]
+    if current_coords[1] == 3 or Map[current_coords[0]][current_coords[1]+1] == "N":
+        surrounding_coords[3] = "None"
+    else: 
+        #East
+        surrounding_coords[3] = Locations_Dict[Map[current_coords[0]][current_coords[1]+1]]["Name"]
+    print(surrounding_coords)
+    North = f"\nNorth of you is {surrounding_coords[0]} "
+    South = f"\nSouth of you is {surrounding_coords[1]} "
+    West =  f"\nWest of you is {surrounding_coords[2]} "
+    East =  f"\nEast of you is {surrounding_coords[3]} "
+    Directions = [North,South,West,East]
+    User_Options_Index = []
+    options:str = ""
+    for i in Directions:
+        if not (surrounding_coords[Directions.index(i)] == "None"):
+            options = options + i
+            User_Options_Index.append(Directions.index(i))
+    options = options + "\n"
+    def check_options(num):
+        ans = False
+        for p in User_Options_Index:
+            if num == p:
+                ans = True
+        return ans 
+    user_choice = input(f"\nSurrounding Destinations:\n{options}\nPress Enter to replay the location or\nChoose a cardinal direction to move to: ")
+    User_Reponses = [["North",[-1,0]],["South",[1,0]],["West",[0,-1]],["East",[0,1]]]
+    Found = False
+    
+    for j in User_Reponses:
+        if not (user_choice.find(j[0]) == -1) and (check_options(User_Reponses.index(j))):
+            next_location_id = Map[current_coords[0]+j[1][0]][current_coords[1]+j[1][1]]
+            Next_Stop = next_location_id
+            Found = True
+            break
+    if not Found:
+        print("\nReplaying previous stadium due to invalid command or enter key\n")
         
-        
+            
+    
+
 def MiniStop_Printer(inp,out):
     global m_cnt
     m = input(f"{inp}")
     Command_Check(m)
     m_cnt = m_cnt + 1
     print(f"{out}")
+
 def Begin(msg=begin):
-    global cnt
+    global cnt,Start
     if  cnt == 1:
         msg = msg_1
     elif cnt == 11:
         msg = end
-    
-    b = input(f"{msg}\n")
-    Command_Check(b)
+    user_response = input(f"{msg}\n")
+    Command_Check(user_response)
     
 def Message_Printer(m):
     global user_name
@@ -163,60 +221,70 @@ Locations_Dict = {
         "Was_Visited" : False
     },
     1 : {
+        "Name" : "Old Trafford",
         "Message" : Game_Msg_List[1],
         "Input_Text" : Game_Ministop_List[1][0],
         "Output_Text" : Game_Ministop_List[1][1],
         "Was_Visited" : False
     },
     2 : {
+        "Name" : "Ethiad",
         "Message" : Game_Msg_List[2],
         "Input_Text" : Game_Ministop_List[2][0],
         "Output_Text" : Game_Ministop_List[2][1],
         "Was_Visited" : False
     },
     3 : {
+        "Name" : "Villa Park",
         "Message" : Game_Msg_List[3],
         "Input_Text" : Game_Ministop_List[3][0],
         "Output_Text" : Game_Ministop_List[3][1],
         "Was_Visited" : False
     },
     4 : {
+        "Name" : "Emirates",
         "Message" : Game_Msg_List[4],
         "Input_Text" : Game_Ministop_List[4][0],
         "Output_Text" : Game_Ministop_List[4][1],
         "Was_Visited" : False
     },
     5 : {
+        "Name" : "Anfield",
         "Message" : Game_Msg_List[5],
         "Input_Text" : Game_Ministop_List[5][0],
         "Output_Text" : Game_Ministop_List[5][1],
         "Was_Visited" : False
     },
     6 : {
+        "Name" : "King Power",
         "Message" : Game_Msg_List[6],
         "Input_Text" : Game_Ministop_List[6][0],
         "Output_Text" : Game_Ministop_List[6][1],
         "Was_Visited" : False
     },
     7 : {
+        "Name" : "Stamford Bridge",
         "Message" : Game_Msg_List[7],
         "Input_Text" : Game_Ministop_List[7][0],
         "Output_Text" : Game_Ministop_List[7][1],
         "Was_Visited" : False
     },
     8 : {
+        "Name" : "Goodison Park",
         "Message" : Game_Msg_List[8],
         "Input_Text" : Game_Ministop_List[8][0],
         "Output_Text" : Game_Ministop_List[8][1],
         "Was_Visited" : False
     },
     9 : {
+        "Name" : "Selhurst Park",
         "Message" : Game_Msg_List[9],
         "Input_Text" : Game_Ministop_List[9][0],
         "Output_Text" : Game_Ministop_List[9][1],
         "Was_Visited" : False
     },
     10 : {
+        "Name" : "London Stadium",
         "Message" : Game_Msg_List[10],
         "Input_Text" : Game_Ministop_List[10][0],
         "Output_Text" : Game_Ministop_List[10][1],
@@ -230,9 +298,13 @@ def Game_Begin():
     Progress_Display()
     Begin()
 def Story_Sequence(num):
+    global Start
     Message_Printer(Locations_Dict[num]["Message"])    
-    Count_Update()
-    Progress_Display(1)
+    if Start:
+        Start = False
+    else:
+        Count_Update()
+        Progress_Display(1)
     MiniStop_Printer(Locations_Dict[num]["Input_Text"],Locations_Dict[num]["Output_Text"])
     Begin()
 def Normal_Ending():
@@ -259,7 +331,8 @@ def main():
                 break
             else:
                 # Replace with next stop when done
-               Story_Sequence(i)
+               Story_Sequence(Next_Stop)
+               Find_Next_Stop()
     if not Quit_Status:
         Normal_Ending()
     elif Quit_Status:
